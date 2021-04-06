34d29702843ee0613987f6ce4701e29d9175dec5
Kaitlyn Katz <kkatz@coveo.com>
2021-04-06 10:06:33 -0400

    remove size sort criteria

--- src/controllers/sort-criteria.tsx
+++ src/controllers/sort-criteria.tsx
@@ -1,9 +1,7 @@
-import { SortCriterion, buildRelevanceSortCriterion, buildDateSortCriterion, SortOrder, buildFieldSortCriterion } from "@coveo/headless";
+import { SortCriterion, buildRelevanceSortCriterion, buildDateSortCriterion, SortOrder } from "@coveo/headless";
 
 export const criteria: [string, SortCriterion][] = [
   ['Relevance', buildRelevanceSortCriterion()],
   ['Date (Ascending)', buildDateSortCriterion(SortOrder.Ascending)],
   ['Date (Descending)', buildDateSortCriterion(SortOrder.Descending)],
-  ['Size (Ascending)', buildFieldSortCriterion('size', SortOrder.Ascending)],
-  ['Size (Descending)', buildFieldSortCriterion('size', SortOrder.Descending)],
 ];
