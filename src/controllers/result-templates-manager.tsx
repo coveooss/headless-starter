1dc31d35d5511270667482fac2b8c1c8f7ef0cac
Kaitlyn Katz <kkatz@coveo.com>
2021-04-28 12:22:16 -0400

    result template exercises

--- src/controllers/result-templates-manager.tsx
+++ src/controllers/result-templates-manager.tsx
@@ -14,6 +14,7 @@ export const resultTemplatesManager: ResultTemplatesManager<
 resultTemplatesManager.registerTemplates(
   {
     conditions: [],
+    fields: ['date'],
     content: (result: Result) => (
       <li key={result.uniqueId}>
         <article>
@@ -23,6 +24,9 @@ resultTemplatesManager.registerTemplates(
             </InteractiveResult>
           </h2>
           <p>{result.excerpt}</p>
+          {result.raw.date && (
+            <p>Last Modified: {new Date(result.raw.date).toLocaleString()}</p>
+          )}
         </article>
       </li>
     ),
@@ -45,18 +49,53 @@ resultTemplatesManager.registerTemplates(
           </div>
           <div>
             <img
-              src={
-                ResultTemplatesHelpers.getResultProperty(
-                  result,
-                  'ytthumbnailurl'
-                ) as string
-              }
+              src={result.raw.ytthumbnailurl as string}
               alt="Thumbnail"
             ></img>
+          </div>
+          <div>
+            {(result.raw.ytvideoduration as number) && (
+              <p>
+                Length: {formatSeconds(result.raw.ytvideoduration as number)}
+              </p>
+            )}
+            {(result.raw.ytlikecount as number) && (
+              <p>Likes: {result.raw.ytlikecount}</p>
+            )}
           </div>
         </article>
       </li>
     ),
-    fields: ['ytthumbnailurl'],
+    fields: ['ytthumbnailurl', 'ytvideoduration', 'ytlikecount'],
+  },
+  {
+    priority: 1,
+    conditions: [
+      ResultTemplatesHelpers.fieldMustMatch('sourcetype', ['GoogleDrive']),
+    ],
+    content: (result: Result) => (
+      <li key={result.uniqueId}>
+        <article>
+          <h2>
+            <InteractiveResult result={result}>
+              {result.title}
+            </InteractiveResult>
+          </h2>
+          <p>{result.excerpt}</p>
+          {result.raw.date && (
+            <p>Last Modified: {new Date(result.raw.date).toLocaleString()}</p>
+          )}
+          {result.raw.size && (
+            <p>File Size: {(result.raw.size / 1000000).toFixed(2)} MB</p>
+          )}
+        </article>
+      </li>
+    ),
+    fields: ['date', 'size'],
   }
 );
+
+function formatSeconds(seconds: number) {
+  const date = new Date(seconds * 1000).toISOString();
+  return seconds < 3600 ? date.substr(14, 5) : date.substr(11, 8);
+}
